bae1579464f9943ac8ad79a7a1116f542e2f92a0
Youssef Nafaa <nafaayoussef@gmail.com>
2021-11-26 07:18:26 -0500

    join tables

--- students.js
+++ students.js
@@ -7,14 +7,17 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-
 pool.query(`
-SELECT students.id, students.name, cohort_id
+SELECT students.id, students.name AS student_name, cohorts.name AS cohort_name 
 FROM students 
+JOIN cohorts
+ON students.cohort_id = cohorts.id
 LIMIT 5;
 `)
   .then(res => {
     res.rows.forEach(user => {
-      console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort_id} cohort`);
-    })
-  });
+      console.log(`${user.id} ${user.student_name} ${user.cohort_name}`)
+    });
+  })
+  .catch(err => console.error('query error', err.stack)
+);
